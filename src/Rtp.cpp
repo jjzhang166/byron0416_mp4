1cb23e0fd4797ddd10d70740d1346a992840bf45
byron <byron0416@gmail.com>
2013-08-25 17:09:17 +0800

    Check out Rtp.h/cpp.

---
+++ src/Rtp.cpp
@@ -0,0 +1,6 @@
+#include "Rtp.h"
+
+
+CRtpPlayer::~CRtpPlayer()
+{
+}
---
+++ src/Rtp.h
@@ -0,0 +1,38 @@
+#ifndef RTP_H
+#define RTP_H
+
+
+#include <string>
+#include <vector>
+
+
+using std::string;
+using std::vector;
+
+#define RTP_MTU 1460
+
+class CRtpPacket
+{
+public:
+	size_t m_Len;
+	char m_Packet[RTP_MTU];
+};
+
+class CRtpSample
+{
+public:
+	size_t m_Timestamp;
+	vector<CRtpPacket> m_Packets;
+};
+
+class CRtpPlayer
+{
+public:
+	virtual ~CRtpPlayer() = 0;
+public:
+	virtual bool Setup(const string&) = 0;
+	virtual bool Play(int) = 0;
+};
+
+
+#endif
